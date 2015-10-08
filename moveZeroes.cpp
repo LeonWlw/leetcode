c1d6f9756d9a7bbd3d992071c81f2e5dfe9549b5
LeonWlw <weiliuwei2013@sina.com>
2015-10-08 10:51:04 +0800

    leetcode283

---
+++ moveZeroes.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void moveZeroes(vector<int>& nums) {
+    if (nums.size() == 0)
+	return;
+    int start = 0;
+    int count = 0;
+    for (int i = 0; i < nums.size(); ++i) {
+	if (nums[i] != 0)
+	    nums[start++] = nums[i];
+	else
+	    count++;
+    }
+    for (int i = nums.size() - count; i < nums.size(); ++i)
+	nums[i] = 0;
+}
+
+int main()
+{
+    vector<int> nums;
+    int tmp;
+    char c;
+    cout << "Please input the array:";
+    while (cin >> tmp) {
+	nums.push_back(tmp);
+	if ((c = cin.get()) == '\n')
+	    break;
+    }
+    for (int i = 0; i < nums.size(); ++i)
+	cout << nums[i] << " ";
+    cout << endl;
+    moveZeroes(nums);
+    for (int i = 0; i < nums.size(); ++i)
+	cout << nums[i] << " ";
+    cout << endl;
+
+    return 0;
+}
