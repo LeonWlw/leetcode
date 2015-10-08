e5b891902b1894d9e0ca49af0f2668aae8bd6205
LeonWlw <weiliuwei2013@sina.com>
2015-10-08 11:14:31 +0800

    leetcode283

--- moveZeroes.cpp
+++ moveZeroes.cpp
@@ -6,8 +6,7 @@ using namespace std;
 void moveZeroes(vector<int>& nums) {
     if (nums.size() == 0)
 	return;
-    int start = 0;
-    int count = 0;
+    int start = 0, count = 0;
     for (int i = 0; i < nums.size(); ++i) {
 	if (nums[i] != 0)
 	    nums[start++] = nums[i];
