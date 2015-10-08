ed89cf9b08e62989f3f5b512c5a246e12eb96214
LeonWlw <weiliuwei2013@sina.com>
2015-10-08 15:39:28 +0800

    leetcode230

---
+++ kthSmallestInBSTNorecursion.cpp
@@ -0,0 +1,22 @@
+//
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+	if (root == NULL)
+	    return 0;
+	stack<TreeNode*> stk;
+	TreeNode* p = root;
+	while (p || !stk.empty()) {
+	    while (p) {
+		stk.push(p);
+		p = p->left;
+	    }
+	    p = stk.top();
+	    if (--k == 0)
+		return p->val;
+
+	    stk.pop();
+	    p = p->right;
+	}
+    }
+};
