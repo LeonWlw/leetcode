158e5caf4184c9fcc171360c46ba3c9a2fee7c57
LeonWlw <weiliuwei2013@sina.com>
2016-01-04 16:25:59 +0800

    1

---
+++ combinationSum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+	vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+		vector<vector<int>> res;
+		vector<int> tmp;
+
+		sort(candidates.begin(), candidates.end());
+		DFS(candidates, target, 0, tmp, res);
+		return res;
+	}
+	void DFS(vector<int>& candidates, int target, int index, vector<int>& tmp, vector<vector<int>>& res) {
+		if (target < 0)
+			return;
+		if (target == 0) {
+			res.push_back(tmp);
+			return;
+		}
+		for (int i = index; i < candidates.size(); ++i) {
+			tmp.push_back(candidates[i]);
+			DFS(candidates, target - candidates[i], i, tmp, res);
+			tmp.pop_back();
+		}
+	}
+};
